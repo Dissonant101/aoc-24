d23cc5ecc9af7751a5de36019d19cad85329cb8e
Dissonant101 <92066010+Dissonant101@users.noreply.github.com>
2025-11-30 22:28:41 -0500

    feat: day 3 part 2

--- day03/main.py
+++ day03/main.py
@@ -3,9 +3,8 @@ import re
 
 def part1(input_data):
     total = 0
-
-    mul_pattern = r"mul\((-?\d+),(-?\d+)\)"
-    matches = re.findall(mul_pattern, input_data)
+    regex_pattern = r"mul\((-?\d+),(-?\d+)\)"
+    matches = re.findall(regex_pattern, input_data)
 
     for x_str, y_str in matches:
         x = int(x_str)
@@ -17,6 +16,18 @@ def part1(input_data):
 
 def part2(input_data):
     total = 0
+    regex_pattern = r"(do\(\))|(don't\(\))|(mul\((-?\d+),(-?\d+)\))"
+    matches = re.findall(regex_pattern, input_data)
+    state = True
+
+    for match in matches:
+        if match[0]:  # do()
+            state = True
+        elif match[1]:  # don't()
+            state = False
+        elif match[2]:  # mul(x,y)
+            if state:
+                total += int(match[3]) * int(match[4])
 
     return total
 
